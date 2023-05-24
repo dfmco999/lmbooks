d536e4238298c0e3cd5086711d1c38e33d986865
D <diego.mantilla.trabajo@gmail.com>
2023-05-23 20:43:38 -0500

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Book;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call('UsersTableSeeder');
+        Book::factory(50)->create();
+    }
+}
